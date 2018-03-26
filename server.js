786e5a08be3dabac459a30eaa2efdf50fc7647f0
Alina L <alodahl@pointloma.edu>
2018-03-25 22:02:13 -0700

    change endpoints so POST / is not protected

--- server.js
+++ server.js
@@ -27,51 +27,59 @@ const jwtCheck = jwt({
   algorithms: ["RS256"]
 });
 
+mongoose.Promise = global.Promise;
+
 nextApp.prepare().then(() => {
   app.use(morgan("common"));
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-  app.use("/api/users", jwtCheck, usersRouter);
+  app.use("/api/users/pick-a-mentee", jwtCheck, usersRouter);
+  app.use("/api/users/:authId", jwtCheck, usersRouter);
+  app.use("/api/users/:userId", jwtCheck, usersRouter);
+  app.use("/api/users/:mentorId/:menteeId", jwtCheck, usersRouter);
   app.use("/api/help", jwtCheck, helpRouter);
+  app.use("/api/users/", usersRouter);
 
   app.get("*", (req, res) => {
     handle(req, res);
   });
+});
 
-  function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+let server;
+
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on("error", err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+      server.close(err => {
         if (err) {
           return reject(err);
         }
-        server = app
-          .listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve();
-          })
-          .on("error", err => {
-            mongoose.disconnect();
-            reject(err);
-          });
+        resolve();
       });
     });
-  }
+  });
+}
 
-  function closeServer() {
-    return mongoose.disconnect().then(() => {
-      return new Promise((resolve, reject) => {
-        console.log("Closing server");
-        server.close(err => {
-          if (err) {
-            return reject(err);
-          }
-          resolve();
-        });
-      });
-    });
-  }
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
 
-  if (require.main === module) {
-    runServer().catch(err => console.error(err));
-  }
-});
+module.exports = { app, runServer, closeServer };
